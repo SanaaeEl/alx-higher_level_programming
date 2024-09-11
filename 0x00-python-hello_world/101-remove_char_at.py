5b7b99c5e9140d613b900b319a5c24f97fed46da
SanaaeEl <assanaae@gmail.com>
2024-09-11 19:57:46 +0100

    remove char at

--- 0x00-python-hello_world/101-remove_char_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    for i, ch in enumerate(str):
-        if (i == n):
-            continue
-        else:
-            copy = copy + ch
-    return copy
