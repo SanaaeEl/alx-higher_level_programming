f0889e9664dda3f165de702931402cfca516b41a
SanaaeEl <assanaae@gmail.com>
2024-09-11 17:52:07 +0100

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,17 +8,15 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *node1, *node2;
+	listint_t *node1 = list, *node2 = list;
 
-	node1 = list;
-	node2 = list;
 	if (!list)
 		return (0);
 
-	while (node2 && node2->next)
+	while (node2 != NULL && node2->next != NULL)
 	{
-		node1 = list->next;
-		node2 = list->next->next;
+		node1 = node1->next;
+		node2 = node2->next->next;
 		if (node1 == node2)
 			return (1);	
 	}
