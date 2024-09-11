e79901b652b2a04b4407397d1f286c84ba038afb
SanaaeEl <assanaae@gmail.com>
2024-09-11 05:56:07 +0100

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for char in range(ord('z'), ord('a') - 1, -1):
-    upper = 32 if char % 2 != 0 else diff = 0
+    if char % 2 = 0:
+        upper = 32
+    else:
+        upper = 0
 print("{}".format(chr(char - upper)), end="")
