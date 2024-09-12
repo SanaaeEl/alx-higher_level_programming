2d5b07ee5e0facaf2ccda0ffa8d930c6a814359a
SanaaeEl <assanaae@gmail.com>
2024-09-12 18:01:33 +0100

    err2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,9 @@ if __name__ == "__main__":
     ln = len(sys.argv) - 1
     if ln == 0:
         print("0 arguments")
-    elif ln > 1:
-        if ln == 1:
+    elif ln == 1:
             print("{} argument:".format(ln))
-        else:
+    else:
             print("{} arguments:".format(ln))
-        for i, arg in enumerate(sys.argv):
-            if (i > 0):
-                print("{}: {}".format(i, arg))
+    for i in range(1, ln+1):
+            print("{}: {}".format(i, sys.argv[i]))
