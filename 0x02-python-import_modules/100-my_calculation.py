d6cb614f51f3e9754d2c2e47e4bc23788ed0a87a
SanaaeEl <assanaae@gmail.com>
2024-09-12 16:45:17 +0100

    my calculator

---
+++ 0x02-python-import_modules/100-my_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    
+    calcul = {'+': add,'-': sub,'*': mul,'/': div}
+    
+    if len(sys.argv) < 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif sys.argv[2] not in calcul:
+        print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        op = sys.argv[2]
+        
+        print("{} {} {} = {}".format(a, op, b, calcul[op](a, b)))
