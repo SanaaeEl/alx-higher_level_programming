585eee8689a46440ccd9dbc71f6b430f4f0e07e6
SanaaeEl <assanaae@gmail.com>
2024-09-12 16:57:47 +0100

    err

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-import sys
-l = len(sys.argv) - 1
-if l == 0:
-    print("0 arguments")
-elif l > 1:
-    if l == 1:
-        print("{} argument:".format(l))
-    else:
-        print("{} arguments:".format(l))
-    for i, arg in enumerate(sys.argv):
-        if (i > 0):
-            print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    import sys
+    
+    l = len(sys.argv) - 1
+    if l == 0:
+        print("0 arguments")
+    elif l > 1:
+        if l == 1:
+            print("{} argument:".format(l))
+        else:
+            print("{} arguments:".format(l))
+        for i, arg in enumerate(sys.argv):
+            if (i > 0):
+                print("{}: {}".format(i, arg))
