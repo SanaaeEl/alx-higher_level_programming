a9abde8c7b2e557c39bf9312ab3e9a5484873435
SanaaeEl <assanaae@gmail.com>
2024-10-30 14:13:49 +0100

    pos

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-
-"""Defines a class square (with methods and instance attributes)."""
+"""Define a class square
+(with methods and instance attributes).
+"""
 
 
 class Square:
     """ Class that defines a square by size and position."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes Square instances
+        """Initializes Square instances.
         Args:
             size (int): size of the square.
             position (tuple): position of the square."""
