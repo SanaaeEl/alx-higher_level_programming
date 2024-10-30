ef18d303e4389e02919258b9f8df84021dcb3d24
SanaaeEl <assanaae@gmail.com>
2024-10-30 10:49:05 +0100

    position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+"""Defines a class square"""
+
+
+class Square:
+    """ Class that defines a square by size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes Square instances"""
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, pos):
+        if isinstance(pos, tuple) and len(pos) == 2
+        and all(isinstance(i, int) and i > 0 for i in pos):
+            self.__position = pos
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__position[1]):
+                print("")
+            for a in range(self.__size):
+                for i in range(self.__position[0]):
+                    print(" ", end="")
+                for b in range(self.__size):
+                    print("#", end="")
+                print()
