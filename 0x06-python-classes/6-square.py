c1a8708d2499fa7ce57a8706f839f9b383d1a1c7
SanaaeEl <assanaae@gmail.com>
2024-10-30 14:42:23 +0100

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,3 +21,35 @@ class Square:
         if s < 0:
             raise ValueError('size must be >= 0')
         self.__size = s
+
+    @property
+    def position(self):
+        """Retrieves an instances's position."""
+        return self.__position
+
+    @position.setter
+    def position(self, pos):
+        """Setts an instances's position."""
+        if isinstance(pos, tuple) and len(pos) == 2
+        and all(isinstance(i, int) and i >= 0 for i in pos):
+            self.__position = pos
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character #."""
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.position[1]):
+                print("")
+            for a in range(self.__size):
+                for i in range(self.__position[0]):
+                    print(" ", end="")
+                for b in range(self.__size):
+                    print("#", end="")
+                print()
